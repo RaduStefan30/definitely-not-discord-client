b7d32d509b86bcb692e069770d9360f6ea46a010
RaduStefan30 <radustefan3010@gmail.com>
2022-11-15 17:34:57 +0200

    add login media queries

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { store } from "./store/store";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import "./queries.css";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
